2c9000ad621bc7b25b34b441b813f763b8c55c47
Vasile Midrigan <111rigan111@gmail.com>
2022-07-24 14:16:56 +0100

    Add Header Component Styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import React from "react";
 export default function Header() {
   return (
     <div className="Header">
-      <h1>T O D O </h1>
-      <div className="Header__theme-switcher"></div>
+      <div className="Header__content">
+        <h1 className="Header__content__h1">T O D O </h1>
+        <div className="Header__content__theme-switcher"></div>
+      </div>
     </div>
   );
 }
